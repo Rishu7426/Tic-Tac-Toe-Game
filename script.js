103034ea7ba917cc49550e59ce10fe5cdaa170e1
Rishu Raj <107173969+Rishu7426@users.noreply.github.com>
2025-02-15 23:05:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ h1 {
   height: 18vmin;
   width: 18vmin;
   background-color: yellow;
-  box-shadow: 1px 4px 2px rgb(0, 0, 0);
+  box-shadow: 1px 4px 2px rgb(255, 220, 230);
     border-radius: 50%;
   border: none;
   background: radial-gradient(210.81% 217.96% at -15.47% 91.8%, rgba(196,196,196,0.2)0%, rgba(255,255,255,.2)100% );
