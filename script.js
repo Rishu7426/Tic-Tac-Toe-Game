11d3db1063d736090eaa4b62d1073345e423ae99
Rishu Raj <107173969+Rishu7426@users.noreply.github.com>
2025-02-15 19:44:20 +0530

    Update script.js, Inner div shadow

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ h1 {
   align-items: center;
   gap: 1.5vmin;
   border: none;
-  box-shadow: 0px 0px 10px rgba(200, 200, 200, .5);
+  box-shadow:inset 0px 0px 100px rgba(200, 200, 200, .5);
 }
 
 .box {
