a140077a9f34667ed6054e17b78f0d03d120e747
Rishu Raj <107173969+Rishu7426@users.noreply.github.com>
2025-02-18 03:16:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ h1 {
   gap: 1.5vmin;
   border: 1px solid orange;
   background-color: rgba(0,0,0,.5);
-  box-shadow:inset 0px 0px 40px rgba(255,255,255,.3);
+  box-shadow:inset 0px 0px 40px rgba(0,0,190,.3);
 }
 
 .box {
